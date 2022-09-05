8c03c7ce1859bd14d1f8c5ad54a72455d6774589
zikaloai71 <Ze.Lo.Moukhtar@el-eng.menofia.edu.eg>
2022-09-06 00:48:18 +0200

    edit features added

--- script.js
+++ script.js
@@ -1,122 +1,172 @@
-const taskHeads =  ["name", "age", 'activeStatus']
-const addForm = document.querySelector("#addForm")
-const dataWrap = document.querySelector("#dataWrap")
-const check =document.getElementById("flexSwitchCheckDefault")
+const taskHeads = ["name", "age", "activeStatus"];
+const addForm = document.querySelector("#addForm");
+const editForm = document.querySelector("#editForm");
+const dataWrap = document.querySelector("#dataWrap");
+const check = document.getElementById("flexSwitchCheckDefault");
+
 // const single = document.querySelector("#single")
 
+const readFromStorage = (key = "users", dataType = "array") => {
+  let data;
+  try {
+    data = JSON.parse(localStorage.getItem(key)) || [];
+    if (!Array.isArray(data) && dataType == "array")
+      throw new Error("data is not an array");
+  } catch (e) {
+    data = [];
+  }
+  return data;
+};
 
-const readFromStorage = (key= "users", dataType="array") => {
-    let data
-    try{
-        data = JSON.parse(localStorage.getItem(key)) || []
-        if(!Array.isArray(data) && dataType=="array") throw new Error("data is not an array")
-    }
-    catch(e){
-        data = []
-    }
-    return data
-}
+const editFromStorage =(users,user)=>{
 
-const writeToStorage = (data, key="users") => {
-    localStorage.setItem(key, JSON.stringify(data))
-}
-
-const createUserObject = (addForm) =>{
-    let user = { id:Date.now() }
-    taskHeads.forEach(head => {
-        if(head==='activeStatus'){
-            if (check.checked){
-                user[head]="active"
-            }
-            else{
-                user[head]="not active"
-            }
-        }
-        else{
-            user[head]= addForm.elements[head].value
-        }
-        
-    })
-    return user 
-}
-const createMyOwnEle = (eleTag, parent, txtContent=null, classes=null) =>{
-    const myNewElement = document.createElement(eleTag)
-    if(classes)  myNewElement.classList = classes
-    if(txtContent) myNewElement.innerText= txtContent
-    parent.appendChild(myNewElement)
-    return myNewElement
-}
-const delUser = (users, i)=>{
-    users.splice(i,1)
-    writeToStorage(users)
-    draw(users)
+ let index= users.findIndex(u=>u.id===user.id)
+ users.splice(index,1,user)
+ writeToStorage(users)
 }
 
-const changeActivity = (users, i)=>{
-    
-    if( users[i].activeStatus==="active"){
-        users[i].activeStatus="not active"
+const writeToStorage = (data, key = "users") => {
+  localStorage.setItem(key, JSON.stringify(data));
+};
+
+const createUserObject = (addForm) => {
+  let user = { id: Date.now() };
+  taskHeads.forEach((head) => {
+    if (head === "activeStatus") {
+      if (check.checked) {
+        user[head] = "active";
+      } else {
+        user[head] = "not active";
+      }
+    } else {
+      user[head] = addForm.elements[head].value;
     }
-    else if ( users[i].activeStatus==="not active"){
-        users[i].activeStatus="active"
-    }
-    writeToStorage(users)
-    draw(users)
-}
+  });
+  return user;
+};
+const createMyOwnEle = (eleTag, parent, txtContent = null, classes = null) => {
+  const myNewElement = document.createElement(eleTag);
+  if (classes) myNewElement.classList = classes;
+  if (txtContent) myNewElement.innerText = txtContent;
+  parent.appendChild(myNewElement);
+  return myNewElement;
+};
+const delUser = (users, i) => {
+  users.splice(i, 1);
+  writeToStorage(users);
+  draw(users);
+};
+
+const changeActivity = (users, i) => {
+  if (users[i].activeStatus === "active") {
+    users[i].activeStatus = "not active";
+  } else {
+    users[i].activeStatus = "active";
+  }
+  writeToStorage(users);
+  draw(users);
+};
+
+// function readElement(id){
+//   let users=readFromStorage();
+//   let user =users.find((user)=>user.id===id)
+//   return user
+// }
+
+const editUser = (user) => {
+  writeToStorage(user, "user");
+  window.location.href = "editUser.html";
+};
+
 // const showSingle = (user)=>{
 //     // localStorage.setItem("itemId", i)
 //     writeToStorage(user , "item")
 //     window.location.href = "single.html"
 // }
 const draw = (users) => {
-    dataWrap.innerHTML=""
-    if(users.length==0){
-        let tr = createMyOwnEle("tr", dataWrap, null, "alert alert-danger")
-        let td = createMyOwnEle("td", tr, "no data found", "alert alert-danger")
-        td.setAttribute("colspan", "5")
-    }
-    users.forEach((user, i)=>{
-        let tr = createMyOwnEle("tr", dataWrap)
-        createMyOwnEle("td", tr, user.id)
-        createMyOwnEle("td", tr, user.name)
-        createMyOwnEle("td", tr, user.age)
-        createMyOwnEle("td", tr, user.activeStatus)
-        let td = createMyOwnEle("td", tr)
-        let statusBtn = createMyOwnEle("button", td, "change activity", "btn btn-primary mx-2 my-1")
-        statusBtn.addEventListener("click", ()=>changeActivity(users, i))
-        let editBtn = createMyOwnEle("button", td, "edit", "btn btn-warning mx-2")
-        let showBtn = createMyOwnEle("button", td, "show", "btn btn-success mx-2")
-        let delBtn = createMyOwnEle("button", td, "delete", "btn btn-danger mx-2")
-        delBtn.addEventListener("click", ()=>delUser(users, i) )
-        
-    })
-}
+  dataWrap.innerHTML = "";
+  if (users.length == 0) {
+    let tr = createMyOwnEle("tr", dataWrap, null, "alert alert-danger");
+    let td = createMyOwnEle("td", tr, "no data found", "alert alert-danger");
+    td.setAttribute("colspan", "5");
+  }
+  users.forEach((user, i) => {
+    let tr = createMyOwnEle("tr", dataWrap);
+    createMyOwnEle("td", tr, user.id);
+    createMyOwnEle("td", tr, user.name);
+    createMyOwnEle("td", tr, user.age);
+    createMyOwnEle("td", tr, user.activeStatus);
+    let td = createMyOwnEle("td", tr);
+    let statusBtn = createMyOwnEle(
+      "button",
+      td,
+      "change activity",
+      "btn btn-primary mx-2 my-1"
+    );
+    statusBtn.addEventListener("click", () => changeActivity(users, i));
+    let editBtn = createMyOwnEle("button", td, "edit", "btn btn-warning mx-2");
+    editBtn.addEventListener("click", () => editUser(users[i]));
+    let showBtn = createMyOwnEle("button", td, "show", "btn btn-success mx-2");
+    let delBtn = createMyOwnEle("button", td, "delete", "btn btn-danger mx-2");
+    delBtn.addEventListener("click", () => delUser(users, i));
+  });
+};
 
-if(addForm){
-    addForm.addEventListener("submit", function(e){
-        e.preventDefault()
-        const user =createUserObject(this)
-        const users = readFromStorage()
-        users.push(user)
-        writeToStorage(users)
-        window.location.href="index.html"
-    })
-    
-check.addEventListener('click',()=>{
-    if(check.checked){
-       
-        document.getElementById('activeToggle').innerText = "active";
-    }
-    else{
-        document.getElementById('activeToggle').innerText = "not active";
-        
-    }
-})
-}
+if (addForm) {
+  addForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const user = createUserObject(this);
+    const users = readFromStorage();
+    users.push(user);
+    writeToStorage(users);
+    window.location.href = "index.html";
+  });
 
-if(dataWrap) {
-    const users = readFromStorage()
-    draw(users)
+  check.addEventListener("click", () => {
+    if (check.checked) {
+      document.getElementById("activeToggle").innerText = "active";
+    } else {
+      document.getElementById("activeToggle").innerText = "not active";
+    }
+  });
+}
+if (dataWrap) {
+  const users = readFromStorage();
+  draw(users);
+}
+if (editForm) {
+  const userNameEdit= document.getElementById('name')
+  const userAgeEdit= document.getElementById('age')
+  const checkEdit= document.getElementsByClassName('check')[0]
+  const activeToggleEdit= document.getElementById('activeToggleEdit')
+  const user = readFromStorage('user', "object");
+  const users=readFromStorage()
+  userNameEdit.value=user.name;
+  userAgeEdit.value=user.age;
+  if(user.activeStatus==='not active'){
+    checkEdit.removeAttribute("checked")
+    activeToggleEdit.innerText='not active'
+  }
+  else{
+    checkEdit.setAttribute("checked",'checked')
+    activeToggleEdit.innerText='active'
+  }
+  checkEdit.addEventListener("click", () => {
+    if (checkEdit.checked) {
+      activeToggleEdit.innerText = "active";
+    } else {
+      activeToggleEdit.innerText = "not active";
+    }
+  });
+  editForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    user.name=userNameEdit.value
+    user.age=userAgeEdit.value
+    user.activeStatus=activeToggleEdit.innerText
+    editFromStorage(users,user)
+    window.location.href='index.html'
+  })
+
 }
 
 // if(single){
@@ -125,3 +175,6 @@ if(dataWrap) {
 //     if(Array.isArray(user)) createMyOwnEle("div", single, "no data to show", "alert alert-danger")
 //     else createMyOwnEle("div", single, user.title, "alert alert-primary")
 // }
+
+
+//1662414345211
